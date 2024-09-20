2f838c7f7445200a606021c7fa1f41f41066a6e6
PopaGeorge123 <popageo02@gmail.com>
2024-09-20 10:24:41 +0300

    dev : modified config

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ import themes from "daisyui/src/theming/themes";
 
 const config = {
   // REQUIRED
-  appName: "Datapick",
+  appName: "MagSpy",
   // REQUIRED: a short description of your app for SEO tags (can be overwritten)
   appDescription:
-    "I know your pain of checking your favorite products every day. Datapick will help you to track the price of your favorite products and send you an email when the price drops.",
+    "I know your pain of checking your favorite products every day. MagSpy will help you to track the price of your favorite products and send you an email when the price drops.",
   // REQUIRED (no https://, not trialing slash at the end, just the naked domain)
-  domainName: "datapick.app",
+  domainName: "magspy.app",
   crisp: {
     // Crisp website ID. IF YOU DON'T USE CRISP: just remove this => Then add a support email in this config file (mailgun.supportEmail) otherwise customer support won't work.
     id: "",
@@ -89,13 +89,13 @@ const config = {
     // subdomain to use when sending emails, if you don't have a subdomain, just remove it. Highly recommended to have one (i.e. mg.yourdomain.com or mail.yourdomain.com)
     subdomain: "mail",
     // REQUIRED — Email 'From' field to be used when sending magic login links
-    fromNoReply: `Datapick <noreply@mail.datapick.app>`,
+    fromNoReply: `MagSpy <noreply@mail.magspy.app>`,
     // REQUIRED — Email 'From' field to be used when sending other emails, like abandoned carts, updates etc..
-    fromAdmin: `George from Datapick <admin@mail.datapick.app>`,
+    fromAdmin: `George from MagSpy <admin@mail.magspy.app>`,
     // Email shown to customer if need support. Leave empty if not needed => if empty, set up Crisp above, otherwise you won't be able to offer customer support."
-    supportEmail: "Datapick | Support support@mail.datapick.app",
+    supportEmail: "MagSpy | Support support@mail.magspy.app",
 
-    fromAlertBot: `Datapick | Alert <noreply@mail.datapick.app>`,
+    fromAlertBot: `MagSpy | Alert <noreply@mail.magspy.app>`,
     // When someone replies to supportEmail sent by the app, forward it to the email below (otherwise it's lost). If you set supportEmail to empty, this will be ignored.
     forwardRepliesTo: "popageo02@gmail.com",
   },
