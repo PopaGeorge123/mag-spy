8c9ba19c8f5a479ed43c0c91c241780d4cc6e90b
PopaGeorge123 <popageo02@gmail.com>
2024-09-28 19:28:12 +0300

    dev : modified connectMongo Method

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -1,18 +1,45 @@
-import mongoose from "mongoose";
-import User from "@/models/User";
+import mongoose from 'mongoose';
 
 const connectMongo = async () => {
   if (!process.env.MONGODB_URI) {
-    throw new Error(
-      "Add the MONGODB_URI environment variable inside .env.local to use mongoose"
-    );
+      throw new Error(
+        "Add the MONGODB_URI environment variable inside .env.local to use mongoose"
+        );
   }
-  return mongoose
-    .connect(process.env.MONGODB_URI, {
+  // Check if the first connection is already active
+  if (mongoose.connections[0].readyState) {
+    console.log("Connected to Database");
+    return true;
+  }
+
+  // If not connected, establish a new connection
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .catch((e) => console.error("Mongoose Client Error: " + e.message));
+    
+    console.log("Connected to Database");
+    return true;
+  } catch (error) {
+    console.log("Error connecting to database: ", error);
+  }
 };
+
+
+
+// const connectMongo = async () => {
+//   if (!process.env.MONGODB_URI) {
+//     throw new Error(
+//       "Add the MONGODB_URI environment variable inside .env.local to use mongoose"
+//     );
+//   }
+//   return mongoose
+//     .connect(process.env.MONGODB_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     })
+//     .catch((e) => console.error("Mongoose Client Error: " + e.message));
+// };
 
 export default connectMongo;
